48dbcbf42e33aedc3490c05fb8273533fb941207
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2023-04-15 23:27:52 +0100

    feat: Mul
    Added `std::ops::Mul` implementations.

--- ux2-macros/src/lib.rs
+++ ux2-macros/src/lib.rs
@@ -474,6 +474,38 @@ pub fn generate_types(item: proc_macro::TokenStream) -> proc_macro::TokenStream 
                         }
                     }
 
+                    impl std::ops::Mul<&#unsigned_ident> for &#unsigned_ident {
+                        type Output = #unsigned_ident;
+                        fn mul(self, rhs: &#unsigned_ident) -> Self::Output {
+                            let x = self.0 * rhs.0;
+                            debug_assert!((#unsigned_ident::MIN.0..#unsigned_ident::MAX.0).contains(&x));
+                            #unsigned_ident(x)
+                        }
+                    }
+                    impl std::ops::Mul<&#unsigned_ident> for #unsigned_ident {
+                        type Output = #unsigned_ident;
+                        fn mul(self, rhs: &#unsigned_ident) -> Self::Output {
+                            let x = self.0 * rhs.0;
+                            debug_assert!((#unsigned_ident::MIN.0..#unsigned_ident::MAX.0).contains(&x));
+                            #unsigned_ident(x)
+                        }
+                    }
+                    impl<'a> std::ops::Mul<#unsigned_ident> for &'a #unsigned_ident {
+                        type Output = #unsigned_ident;
+                        fn mul(self, rhs: #unsigned_ident) -> Self::Output {
+                            let x = self.0 * rhs.0;
+                            debug_assert!((#unsigned_ident::MIN.0..#unsigned_ident::MAX.0).contains(&x));
+                            #unsigned_ident(x)
+                        }
+                    }
+                    impl std::ops::Mul<#unsigned_ident> for #unsigned_ident {
+                        type Output = #unsigned_ident;
+                        fn mul(self, rhs: #unsigned_ident) -> Self::Output {
+                            let x = self.0 * rhs.0;
+                            debug_assert!((#unsigned_ident::MIN.0..#unsigned_ident::MAX.0).contains(&x));
+                            #unsigned_ident(x)
+                        }
+                    }
 
                     impl std::fmt::Binary for #unsigned_ident {
                         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -777,6 +809,39 @@ pub fn generate_types(item: proc_macro::TokenStream) -> proc_macro::TokenStream 
                         type Output = #signed_ident;
                         fn sub(self, rhs: #signed_ident) -> Self::Output {
                             let x = self.0 - rhs.0;
+                            debug_assert!((#signed_ident::MIN.0..#signed_ident::MAX.0).contains(&x));
+                            #signed_ident(x)
+                        }
+                    }
+
+                    impl std::ops::Mul<&#signed_ident> for &#signed_ident {
+                        type Output = #signed_ident;
+                        fn mul(self, rhs: &#signed_ident) -> Self::Output {
+                            let x = self.0 * rhs.0;
+                            debug_assert!((#signed_ident::MIN.0..#signed_ident::MAX.0).contains(&x));
+                            #signed_ident(x)
+                        }
+                    }
+                    impl std::ops::Mul<&#signed_ident> for #signed_ident {
+                        type Output = #signed_ident;
+                        fn mul(self, rhs: &#signed_ident) -> Self::Output {
+                            let x = self.0 * rhs.0;
+                            debug_assert!((#signed_ident::MIN.0..#signed_ident::MAX.0).contains(&x));
+                            #signed_ident(x)
+                        }
+                    }
+                    impl<'a> std::ops::Mul<#signed_ident> for &'a #signed_ident {
+                        type Output = #signed_ident;
+                        fn mul(self, rhs: #signed_ident) -> Self::Output {
+                            let x = self.0 * rhs.0;
+                            debug_assert!((#signed_ident::MIN.0..#signed_ident::MAX.0).contains(&x));
+                            #signed_ident(x)
+                        }
+                    }
+                    impl std::ops::Mul<#signed_ident> for #signed_ident {
+                        type Output = #signed_ident;
+                        fn mul(self, rhs: #signed_ident) -> Self::Output {
+                            let x = self.0 * rhs.0;
                             debug_assert!((#signed_ident::MIN.0..#signed_ident::MAX.0).contains(&x));
                             #signed_ident(x)
                         }
