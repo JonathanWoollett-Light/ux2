e3dc097b418c8028be346ae58be3b8a9be5fb37c
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-02-02 22:26:07 +0100

    feat: Add Default impl
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

--- ux2-macros/src/lib.rs
+++ ux2-macros/src/lib.rs
@@ -823,6 +823,11 @@ pub fn generate_types(item: proc_macro::TokenStream) -> proc_macro::TokenStream 
             #[repr(transparent)]
             #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash)]
             pub struct #unsigned_ident(#unsigned_inner_ident);
+            impl core::default::Default for #unsigned_ident {
+                fn default() -> Self {
+                    #unsigned_ident::new(#unsigned_inner_ident::default())
+                }
+            }
             impl #unsigned_ident {
                 fn mask(self) -> Self {
                     Self(self.0 & #unsigned_mask.overflowing_sub(#unsigned_one).0)
@@ -1179,6 +1184,11 @@ pub fn generate_types(item: proc_macro::TokenStream) -> proc_macro::TokenStream 
             #[repr(transparent)]
             #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash)]
             pub struct #signed_ident(#signed_inner_ident);
+            impl core::default::Default for #signed_ident {
+                fn default() -> Self {
+                    #signed_ident::new(#signed_inner_ident::default())
+                }
+            }
             impl #signed_ident {
                 fn mask(self) -> Self {
                     let x = if self.0 & (#signed_one << (#size - 1)) == 0 {
